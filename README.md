b327731ca53e69db4811ce1783d0ed9cb84511bd
Alpha Ibrahima Sow <alphaibrahimas95@gmail.com>
2022-02-12 12:38:43 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Ecommerce with Python Django" 
---
+++ category/models.py
@@ -0,0 +1,20 @@
+from distutils.command.upload import upload
+from tabnanny import verbose
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    description = models.TextField(max_length=500, blank=True)
+    cart_image = models.ImageField(upload_to='categories/photos', blank=True)
+
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    
+    def __str__(self):
+        return self.category_name
