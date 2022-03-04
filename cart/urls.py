96cfb9d2617e464c35aba143693864df1295f165
Alpha Ibrahima Sow <alphaibrahimas95@gmail.com>
2022-03-04 21:26:01 +0000

    increment & decrement & remove cart item

--- cart/urls.py
+++ cart/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.cart, name='cart'),
     path('add-cart/<int:product_id>/', views.add_cart, name='add-cart'),
+    path('remove-cart/<int:product_id>/', views.remove_cart, name='remove-cart'),
+    path('remove-cart-item/<int:product_id>/', views.remove_cart_item, name='remove'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,7 @@
 from itertools import product
 from multiprocessing import context
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from store.models import Product
 from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
@@ -14,6 +14,7 @@ def _cart_id(request):
     if not cart:
         cart = request.session.create()
     return cart
+
 
 def add_cart(request, product_id):
     product = Product.objects.get(id=product_id)
@@ -41,6 +42,27 @@ def add_cart(request, product_id):
     return redirect('cart')
 
 
+# allow the customer to reduce the quantity of his order
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+
+    if cart_item.quantite > 1:
+        cart_item.quantite -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('cart')
+
+# 
 def cart(request, total = 0, quantity = 0, cart_items = None):
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
@@ -48,12 +70,17 @@ def cart(request, total = 0, quantity = 0, cart_items = None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantite)
             quantity += cart_item.quantite
+        tax = (2 * total)/100
+        print(tax)
+        global_total = tax + total
     except ObjectDoesNotExist:
         pass
 
     context = {
         'total' : total,
         'quantity' : quantity,
-        'cart_items' : cart_items
+        'cart_items' : cart_items,
+        'tax' : tax,
+        'global_total': global_total
     }
     return render(request, 'store/cart.html', context)
